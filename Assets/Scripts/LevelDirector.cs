5e4f5540b07446760eb8104b00b63154172845d6
Nash Roy <nashroy11@gmail.com>
2021-03-12 14:39:04 -0500

    follow to creation of game director and movement camera

---
+++ Assets/Scripts/LevelDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDirector : MonoBehaviour
+{
+    [SerializeField] public List<Bridge> bridge;
+    [SerializeField] public int bridgeIndex;
+
+    void Start()
+    {
+        if (bridge != null && bridge.Count >= 2)
+        { 
+            //range must be zero to the bridge.length, as the final number isn't inclusive
+            bridgeIndex = Random.Range(0, 2);
+            Debug.Log("Removing Bridge at index: " + bridgeIndex);
+            bridge[bridgeIndex].gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference movementControl;
+    [SerializeField] private InputActionReference jumpControl;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Vector3 playerVelocity;
+    [SerializeField] private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 10.0f;
+    [SerializeField] private float jumpHeight = 10.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    private Transform cameraMainTransform;
+    [SerializeField] private float rotationSpeed = 4f;
+
+    private void OnEnable()
+    {
+        movementControl.action.Enable();
+        jumpControl.action.Enable();
+    }
+    private void OnDisable()
+    {
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
+    }
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMainTransform = Camera.main.transform;
+        LockMouse();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movement.x,0,movement.y);
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+        move.y = 0;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (jumpControl.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        //unbinds camera rotations based off movement
+        if(movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+    private void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
+
