3ebe16552466cd93e7e19de6e7653e8f7835e420
Nash Roy <nashroy11@gmail.com>
2021-03-16 21:52:26 -0400

    time power
    there is a bug where the game slowly speeds up

---
+++ Assets/Scripts/EndlessFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EndlessFall : MonoBehaviour
+{
+
+    public Rigidbody boxbody;
+    public float positionY = 30;
+    public float positionReset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody boxbody = gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(boxbody.transform.position.y <= positionReset)
+        {
+            boxbody.transform.position = new Vector3(boxbody.transform.position.x, positionY, boxbody.transform.position.z);
+            Debug.Log("RESET");
+        }
+
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,64 +1,83 @@
 ﻿using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
-
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference movementControl;
-    [SerializeField] private InputActionReference ActionOne;
+    [SerializeField] private InputActionReference actionOne;
     [SerializeField] private InputActionReference jumpControl;
     [SerializeField] private CharacterController controller;
-    [SerializeField] private GameObject poggers;
     [SerializeField] private Vector3 playerVelocity;
     [SerializeField] private bool groundedPlayer;
+    [SerializeField] private bool timeCheat = false;
     [SerializeField] private float playerSpeed = 10.0f;
     [SerializeField] private float jumpHeight = 10.0f;
     [SerializeField] private float gravityValue = -9.81f;
-  
     [SerializeField] private float rotationSpeed = 4f;
+    [SerializeField] private Canvas UIdisplay;
+
     private Vector3 teleport;
     public bool crossed = false;
     public GameObject player;
     private Transform cameraMainTransform;
+    private Text scoreText;
+    float slowdownFactor = 0.05f;
 
+    //DELEEEEET
+    float totalTime;
+    float scoreTime;
+    float timer;
 
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMainTransform = Camera.main.transform;
+        UIdisplay = (Canvas)FindObjectOfType(typeof(Canvas));
+        scoreText = GameObject.Find("Canvas/Text").GetComponent<Text>();
+    }
 
     private void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
-        //Player = controller.transform.gameObject;
-        cameraMainTransform = Camera.main.transform;
-        LockMouse();
-        StartCoroutine(TimeSlow());
 
+        LockMouse();
+        //StartCoroutine(TimeSlow());
     }
 
     private void FixedUpdate()
     {
+        TimeTracker();
         TeleportPlayer();
     }
 
     void Update()
+    {
+        Move();
+        if (actionOne.action.triggered)
+        {
+            //TimePower();
+        }
+        Debug.Log(Time.timeScale);
+    }
+
+    private void Move()
     {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
-        if (groundedPlayer)
-        {
-            //tbr
-            //playerSpeed = 10;
-        }
 
         Vector2 movement = movementControl.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movement.x,0,movement.y);
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
         move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
         move.y = 0;
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        controller.Move(move * Time.unscaledDeltaTime * playerSpeed);
 
         // Changes the height position of the player..
         if (jumpControl.action.triggered && groundedPlayer)
@@ -66,15 +85,15 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        playerVelocity.y += gravityValue * Time.unscaledDeltaTime;
+        controller.Move(playerVelocity * Time.unscaledDeltaTime);
 
         //unbinds camera rotations based off movement
-        if(movement != Vector2.zero)
+        if (movement != Vector2.zero)
         {
             float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.unscaledDeltaTime * rotationSpeed);
         }
     }
 
@@ -82,6 +101,10 @@ public class PlayerController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+    private void TimePower()
+    {
+        timeCheat = !timeCheat;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -112,17 +135,54 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         movementControl.action.Enable();
+        actionOne.action.Enable();
         jumpControl.action.Enable();
     }
+
     private void OnDisable()
     {
         movementControl.action.Disable();
+        actionOne.action.Disable();
         jumpControl.action.Disable();
+    }
+    
+    //calculates and sets time for player UI
+    private void TimeTracker()
+    {
+        timer += Time.deltaTime;
+        if (timeCheat == true)
+        {
+            //SlowMo();
+        }
+        scoreTime = timer - timer % 1;
+        scoreText.text = scoreTime.ToString();
+        //Debug.Log(timer - timer % 1);
+        
+    }
+
+    private void ResumeTime()
+    {
+        //Time.timeScale = 1f;
+    }
+
+    private void SlowMo()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
     }
 
     IEnumerator TimeSlow()
     {
         yield return new WaitForSeconds(2);
+        Debug.Log("timer: " + totalTime );
+    }
+
+    /// <summary>
+    /// Loads the information from previous levels into the canvas.
+    /// </summary>
+    private void LoadCanvasInformation()
+    {
+
     }
 
 }
