846294996a38a4c07f264df9064939c924d36d24
Nash Roy <nashroy11@gmail.com>
2021-04-11 16:53:37 -0400

    fixing all this malarky

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,47 +5,63 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private CharacterController laser;
+    private bool movingUp = false;
+
+    [Header("Movement")]
     public float movementSpeed;
     public float upperLimit;
     public float lowerLimit;
-    private bool movingUp = false;
-    public float MaxDistance;
+    public bool canMove = false;
+    [Header("Requirements")]
+    public LineRenderer shoot;
+    public Transform firepoint;
+
     Vector3 originalPosition;
-    Collider playerCol;
+    public Collider playerCol;
     RaycastHit hit;
-    bool hitDetect;
+
+
     private void Awake()
     {
         laser = gameObject.GetComponent<CharacterController>();
+        shoot = GameObject.Find("Line").GetComponent<LineRenderer>();
         originalPosition = transform.root.position;
         if (movementSpeed == 0)
         {
             movementSpeed = 1;
         }
+        shoot.enabled = false;
     }
 
     private void FixedUpdate()
     {
-        if (movingUp == true)
+        if (canMove == true)
         {
-            if(transform.root.position.y > originalPosition.y + upperLimit)
+            if (movingUp == true)
             {
-                movingUp = false;
+                if (transform.root.position.y > originalPosition.y + upperLimit)
+                {
+                    movingUp = false;
+                }
+                else
+                    MoveUp();
             }
-            else
-                MoveUp();
-        }
-        else if (movingUp == false)
-        {
-            if(transform.root.position.y < originalPosition.y - lowerLimit)
+            else if (movingUp == false)
             {
-                movingUp = true;
+                if (transform.root.position.y < originalPosition.y - lowerLimit)
+                {
+                    movingUp = true;
+                }
+                else
+                    MoveDown();
             }
-            else
-                MoveDown();
         }
     }
-    
+
+    private void Update()
+    {
+    }
+
     private void MoveDown()
     {
         transform.root.Translate(Vector3.down * movementSpeed * 0.01f);
@@ -59,15 +75,41 @@ public class Laser : MonoBehaviour
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, fwd, out hit, 100))
+        Vector3 fwd = transform.TransformDirection(Vector3.up);
+        bool HitDetect = Physics.BoxCast(playerCol.bounds.center, transform.localScale, fwd, out hit, transform.rotation, 100);
+
+        if (HitDetect)
         {
-            if(hit.collider.tag.Equals("Player"))
+            shoot.enabled = true;
+            //Debug.DrawRay(transform.position, fwd * hit.distance, Color.red);
+            shoot.SetPosition(0, transform.position);
+            shoot.SetPosition(1, hit.point);
+            Debug.Log("hit");
+            Gizmos.DrawRay(playerCol.transform.position, fwd * hit.distance);
+            Gizmos.DrawWireCube(playerCol.transform.position + fwd * hit.distance, transform.localScale);
+            if (hit.collider.tag.Equals("Player"))
             {
-                Debug.Log("HJIT");
-                Debug.DrawRay(transform.position, fwd * hit.distance, Color.red);
-                Debug.Break();
+
             }
+        }    
+
+        /*
+         *         if (Physics.Raycast(transform.position, fwd, out hit, 100))
+        {
+            //if(hit.collider)
+            //{
+    
+                shoot.enabled = true;
+                Debug.DrawRay(transform.position, fwd * hit.distance, Color.red);
+                shoot.SetPosition(0, transform.position);
+                shoot.SetPosition(1, hit.point);
+                Debug.Log("hit");
+            if (hit.collider.tag.Equals("Player"))
+                {
+                    
+                }
+            //}
         }
+         */
     }
 }
